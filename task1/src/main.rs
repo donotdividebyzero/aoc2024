e898bd3779c9588d75e4af68b49041a8b7a0084a
Maciej Vel <donotdividebyzero@pm.me>
2024-12-01 21:49:40 +0100

    second part of task 1

--- task1/src/main.rs
+++ task1/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, Read};
 use std::num::ParseIntError;
@@ -12,7 +13,7 @@ fn read_file(file_path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
-fn aoc_task(input: &str) -> Result<i64, ParseIntError> {
+fn aoc_task(input: &str) -> Result<(i64, i64), ParseIntError> {
     let input: Vec<&str> = input.split('\n').collect();
     let input_len = input.len() - 1;
     let mut left_col: Vec<i64> = Vec::with_capacity(input_len);
@@ -28,14 +29,26 @@ fn aoc_task(input: &str) -> Result<i64, ParseIntError> {
 
     left_col.sort();
     right_col.sort();
+    let mut apperance: HashMap<i64, usize> = HashMap::with_capacity(input_len);
+    for elem in &right_col {
+        apperance.insert(*elem, match apperance.get(elem) {
+            Some(v) => v + 1,
+            None => 1
+        });
+    }
 
     let mut result = 0;
+    let mut similarity_score = 0;
 
     for i in 0..input_len {
         result += (left_col[i] - right_col[i]).abs();
+        similarity_score += left_col[i] * (match apperance.get(&left_col[i]) {
+            Some(v) => *v as i64,
+            None => 0 as i64
+        });
     }
 
-    Ok(result)
+    Ok((result, similarity_score))
 }
 
 
@@ -45,9 +58,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let input = read_file(filename)?;
 
-    let result = aoc_task(input.as_str());
+    let (result, similarity) = aoc_task(input.as_str()).unwrap();
 
-    println!("Result: {}", result.unwrap());
+    println!("Result: {}, Similarity: {}", result, similarity);
 
     Ok(())
 }
