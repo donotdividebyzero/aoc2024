165337ac947ecfc9604baab64cef4f9ab88be6a6
Maciej Vel <donotdividebyzero@pm.me>
2024-12-02 16:12:09 +0100

    some stupid changes because i was bored

--- task2/src/main.rs
+++ task2/src/main.rs
@@ -11,122 +11,96 @@ fn read_file(file_path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
-fn aoc_task1(input: &str) -> Result<i64, ParseIntError> {
-    let input: Vec<&str> = input.split('\n').collect();
-
-    fn direction_way(step: i64) -> i64 {
-        if step > 0 {
-            1
-        } else {
-            -1
-        }
-    }
-
-    fn is_proper_step(step: i64) -> bool {
-        step != 0 && step.abs() <= 3
-    }
-
-    Ok(input
+fn prepare_data(input: &str) -> Vec<Vec<i64>> {
+    input
+        .split('\n')
+        .collect::<Vec<_>>()
         .into_iter()
-        .map(|levels: &str| -> i64 {
-            if levels.is_empty() {
-                return 0;
-            }
-
-            let levels: Vec<i64> = levels
+        .map(|levels: &str| {
+            levels
                 .split_whitespace()
                 .collect::<Vec<_>>()
                 .into_iter()
-                .map(|l| l.parse::<i64>().unwrap())
-                .collect();
-            // RULES:
-            //    The levels are either all increasing or all decreasing.
-            //    Any two adjacent levels differ by at least one and at most three.
-            if levels.len() < 2 {
-                return 0;
-            }
-
-            let levels_count = levels.len() - 1;
-            let main_dir = direction_way(levels[1] - levels[0]);
-
-            for i in 0..levels_count {
-                let step = levels[i + 1] - levels[i];
-
-                let is_proper_step = is_proper_step(step);
-
-                if !is_proper_step || direction_way(step) != main_dir {
-                    return 0;
-                }
-            }
-
-            1
+                .map(|n: &str| n.parse::<i64>().unwrap())
+                .collect()
         })
-        .sum())
+        .collect()
 }
 
-fn aoc_task2(input: &str) -> Result<i64, ParseIntError> {
-    let input: Vec<&str> = input.split('\n').collect();
+fn run_solution(input: &str, solution: impl Fn(Vec<i64>) -> i64) -> Result<i64, ParseIntError> {
+    let data = prepare_data(input);
+    Ok(data.into_iter().map(solution).sum())
+}
 
-    fn direction_way(step: i64) -> i64 {
-        if step > 0 {
-            1
-        } else {
-            -1
+fn direction_way(step: i64) -> i64 {
+    if step > 0 {
+        1
+    } else {
+        -1
+    }
+}
+
+fn is_proper_step(step: i64) -> bool {
+    step != 0 && step.abs() <= 3
+}
+
+fn aoc_task1(levels: Vec<i64>) -> i64 {
+    // RULES:
+    //    The levels are either all increasing or all decreasing.
+    //    Any two adjacent levels differ by at least one and at most three.
+    if levels.len() < 2 {
+        return 0;
+    }
+
+    let levels_count = levels.len() - 1;
+    let main_dir = direction_way(levels[1] - levels[0]);
+
+    for i in 0..levels_count {
+        let step = levels[i + 1] - levels[i];
+
+        let is_proper_step = is_proper_step(step);
+
+        if !is_proper_step || direction_way(step) != main_dir {
+            return 0;
         }
     }
 
-    fn is_proper_step(step: i64) -> bool {
-        step != 0 && step.abs() <= 3
+    1
+}
+
+fn aoc_task2(levels: Vec<i64>) -> i64 {
+    // RULES:
+    //    The levels are either all increasing or all decreasing.
+    //    Any two adjacent levels differ by at least one and at most three.
+    //    You can have one wrong step
+    if levels.len() < 2 {
+        return 0;
     }
 
-    Ok(input
-        .into_iter()
-        .map(|levels: &str| -> i64 {
-            if levels.is_empty() {
-                return 0;
-            }
+    let levels_count = levels.len() - 1;
+    let mut main_dir = 0;
 
-            let levels: Vec<i64> = levels
-                .split_whitespace()
-                .collect::<Vec<_>>()
-                .into_iter()
-                .map(|l| l.parse::<i64>().unwrap())
-                .collect();
-            // RULES:
-            //    The levels are either all increasing or all decreasing.
-            //    Any two adjacent levels differ by at least one and at most three.
-            if levels.len() < 2 {
-                return 0;
-            }
+    let mut incline: u32 = 0;
+    for i in 0..levels_count {
+        let step = levels[i + 1] - levels[i];
 
-            let levels_count = levels.len() - 1;
-            let mut main_dir = 0;
+        let is_proper_step = is_proper_step(step);
 
-            let mut incline: u32 = 0;
-            for i in 0..levels_count {
-                let step = levels[i + 1] - levels[i];
+        if is_proper_step && main_dir == 0 {
+            main_dir = direction_way(step);
+        }
 
-                let is_proper_step = is_proper_step(step);
+        if is_proper_step && direction_way(step) == main_dir {
+            incline |= 1 << i;
+        }
+    }
 
-                if is_proper_step && main_dir == 0 {
-                    main_dir = direction_way(step);
-                }
-
-                if is_proper_step && direction_way(step) == main_dir {
-                    incline |= 1 << i;
-                }
-            }
-
-            let correct_steps = incline.count_ones();
-            if (correct_steps == levels_count as u32)
-                || (correct_steps == (levels_count as u32 - 1))
-            {
-                1
-            } else {
-                0
-            }
-        })
-        .sum())
+    let correct_steps = incline.count_ones();
+    if (correct_steps == levels_count as u32) || (correct_steps == (levels_count as u32 - 1)) {
+        1
+    } else {
+        0
+    }
 }
 
 fn main() {}
@@ -143,7 +117,7 @@ mod tests {
     #[test]
     fn test_sol1() -> Result<(), Box<dyn std::error::Error>> {
         let input = read_file(FILENAME)?;
-        assert_eq!(SOL1, aoc_task1(input.as_str()).unwrap());
+        assert_eq!(SOL1, run_solution(input.as_str(), aoc_task1).unwrap());
 
         Ok(())
     }
@@ -151,7 +125,7 @@ mod tests {
     #[test]
     fn test_sol2() -> Result<(), Box<dyn std::error::Error>> {
         let input = read_file(FILENAME)?;
-        assert_eq!(SOL2, aoc_task2(input.as_str()).unwrap());
+        assert_eq!(SOL2, run_solution(input.as_str(), aoc_task2).unwrap());
 
         Ok(())
     }
